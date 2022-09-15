a1faaea218cb8b0174ab8407bef6a290df010665
BohdanLutskiv <lutskiv2010@gmail.com>
2022-09-15 20:15:53 +0300

    fix bugs/ Connection Layout

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./Footer.module.scss"
+
+const Footer = () => {
+    return (
+        <footer className={classes.footer}>
+            © 2022 Салон краси Magic beauty
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from "../../components/Layout/Layout";
 
 const Index = () => {
     return (
-        <div>
-            
-        </div>
+        <Layout>
+            Test
+        </Layout>
     );
 };
 
